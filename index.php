fc1a4b2c80c49ba6dd61ee688626d6ddbc316f42
Alexander <worldintheworld@mail.ru>
2019-07-30 12:16:37 +0300

    30/07/19

--- index.php
+++ index.php
@@ -1,44 +1,36 @@
 <?php
 
-for($i=0;$i<=999;$i++)
-{
-    $mass[$i]=$mass[$i-1]+3;
+$mass = [];
+
+for ($i = 0; $i <= 999; $i++) {
+    $mass[$i] = $mass[$i - 1] + 3;
 }
 
-if (isset($_POST['rows'])) 
-{ 
-    $result=$_POST['rows'];
+if (isset($_POST['rows'])) {
+    $result = $_POST['rows'];
     $s = 0;
-    $f = count($mass)-1;
-    
-    while($s <= $f)
-    {
-        $q = floor(($s+$f)/2);
-        
-        if($mass[$q] == $result)
-        {
+    $f = count($mass) - 1;
+
+    while ($s <= $f) {
+        $q = floor(($s + $f) / 2);
+
+        if ($mass[$q] == $result) {
             break;
-        }
-        elseif($mass[$q] > $result)
-        {
-            $f = $q-1;
-        }
-        else
-        {
-            $s = $q+1;
+        } elseif ($mass[$q] > $result) {
+            $f = $q - 1;
+        } else {
+            $s = $q + 1;
         }
     }
-    echo $q;    
+    echo $q;
 }
 ?>
-
 
 <form method="POST" action="index.php">
     <select name="rows" size="10">
         <?php
-        for ($i = 0; $i < 1000; $i++)
-        {
-            echo "<option value='" . $mass[$i] . "'>" . $i.'/'.$mass[$i] . "</option>";
+        for ($i = 0; $i < 1000; $i++) {
+            echo "<option value='" . $mass[$i] . "'>" . $i . '/' . $mass[$i] . "</option>";
         }
         ?>
     </select>
